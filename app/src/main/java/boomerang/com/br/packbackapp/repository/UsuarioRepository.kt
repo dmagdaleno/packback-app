a8bf1e2ccaa6f7700b85bf72912707b8360c8080
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-08-19 23:09:10 -0300

    instancia do banco com room

--- app/src/main/java/boomerang/com/br/packbackapp/repository/UsuarioRepository.kt
+++ app/src/main/java/boomerang/com/br/packbackapp/repository/UsuarioRepository.kt
@@ -1,48 +1,57 @@
 package boomerang.com.br.packbackapp.repository
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import boomerang.com.br.packbackapp.domain.Usuario
 import boomerang.com.br.packbackapp.repository.local.UsuarioDao
 import boomerang.com.br.packbackapp.repository.web.UsuarioService
-import java.util.concurrent.Executor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.concurrent.TimeUnit
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class UsuarioRepository @Inject constructor(
-        private val usuarioService: UsuarioService,
-        private val executor: Executor,
-        private val usuarioDao: UsuarioDao
+class UsuarioRepository (
+        private val service: UsuarioService,
+        private val dao: UsuarioDao
 ) {
 
     companion object {
-        val FRESH_TIMEOUT = TimeUnit.DAYS.toMillis(1)
+        private const val TAG = "UsuarioRepository"
+        private val FRESH_TIMEOUT = TimeUnit.DAYS.toMillis(1)
     }
 
     fun getUsuario(id: Long): LiveData<Usuario> {
         atualizaUsuario(id)
-        return usuarioDao.carrega(id)
+        return dao.carrega(id)
     }
 
     private fun atualizaUsuario(id: Long) {
 
-        executor.execute {
-            val usuarioAtualizado = usuarioEstaAtualizado(id)
+        val usuarioAtualizado = usuarioEstaAtualizado(id)
 
-            if (!usuarioAtualizado) {
+        if (!usuarioAtualizado) {
 
-                val response = usuarioService.getUsuario(id).execute()
+            service.getUsuario(1).enqueue(object : Callback<Usuario> {
 
-                val usuario = response.body()
-                        ?: throw IllegalStateException("Usuario não pode ser nulo")
+                override fun onFailure(call: Call<Usuario>, t: Throwable) {
+                    Log.e(TAG, "Erro ao buscar usuário", t)
+                }
 
-                usuarioDao.salva(usuario)
-            }
+                override fun onResponse(call: Call<Usuario>, response: Response<Usuario>) {
+
+                    val usuario = response.body()
+                            ?: throw IllegalArgumentException("Usuário não encontrado")
+
+                    Log.d(TAG, "Encontrado usuário ${usuario.nome}")
+
+                    dao.salva(usuario)
+                }
+
+            })
         }
     }
 
     private fun usuarioEstaAtualizado(id: Long): Boolean {
-        return usuarioDao.carrega(id).value != null
+        return dao.carrega(id).value != null
     }
 }
--- app/src/main/java/boomerang/com/br/packbackapp/repository/local/AppDatabase.kt
+++ app/src/main/java/boomerang/com/br/packbackapp/repository/local/AppDatabase.kt
@@ -1,11 +1,36 @@
 package boomerang.com.br.packbackapp.repository.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import boomerang.com.br.packbackapp.domain.Usuario
 
 @Database(entities = [Usuario::class], version = 1)
-abstract class Database : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun usuarioDao(): UsuarioDao
+
+    companion object {
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            if(instance == null) {
+                synchronized(AppDatabase::class) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            AppDatabase::class.java,
+                            "packback.db")
+                            .build()
+                }
+            }
+
+            return instance!!
+        }
+
+        fun destroyInstance() {
+            instance?.close()
+            instance = null
+        }
+    }
 }
