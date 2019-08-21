45537c75f004d43e7aaef69790da8c18fb464e4a
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-08-20 22:53:14 -0300

    cria ponte entre o repositorio e a activity

--- app/src/main/java/br/com/boomerang/packbackapp/view/model/PerfilUsuarioViewModel.kt
+++ app/src/main/java/br/com/boomerang/packbackapp/view/model/PerfilUsuarioViewModel.kt
@@ -1,15 +1,11 @@
 package br.com.boomerang.packbackapp.view.model
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import br.com.boomerang.packbackapp.domain.Usuario
 import br.com.boomerang.packbackapp.repository.UsuarioRepository
 
-class PerfilUsuarioViewModel (
-        savedStateHandle: SavedStateHandle,
-        repositorio: UsuarioRepository
-): ViewModel() {
-    val id : Long = savedStateHandle["uid"] ?: throw IllegalArgumentException("id não encontrado")
+class PerfilUsuarioViewModel (val id: Long, repositorio: UsuarioRepository): ViewModel() {
+
     val usuario : LiveData<Usuario> = repositorio.getUsuario(id)
 }
