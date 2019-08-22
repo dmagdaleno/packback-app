c7300e4c8102a0d903380575b5fe0158624e1788
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-08-21 22:10:36 -0300

    usa fabrica de viewmodel de usuario

--- app/src/main/java/br/com/boomerang/packbackapp/ui/activity/PerfilUsuarioActivity.kt
+++ app/src/main/java/br/com/boomerang/packbackapp/ui/activity/PerfilUsuarioActivity.kt
@@ -1,15 +1,14 @@
 package br.com.boomerang.packbackapp.ui.activity
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import br.com.boomerang.packbackapp.R
 import br.com.boomerang.packbackapp.domain.Usuario
-import br.com.boomerang.packbackapp.repository.UsuarioRepository
 import br.com.boomerang.packbackapp.repository.local.AppDatabase
-import br.com.boomerang.packbackapp.repository.web.PackbackService
-import br.com.boomerang.packbackapp.repository.web.UsuarioService
 import br.com.boomerang.packbackapp.view.model.PerfilUsuarioViewModel
+import br.com.boomerang.packbackapp.view.model.PerfilUsuarioViewModelFactory
 import kotlinx.android.synthetic.main.activity_perfil_usuario.*
 
 class PerfilUsuarioActivity : AppCompatActivity() {
@@ -18,23 +17,16 @@ class PerfilUsuarioActivity : AppCompatActivity() {
         private const val TAG = "PerfilUsuarioActivity"
     }
 
+    private val viewModel: PerfilUsuarioViewModel by viewModels {
+        PerfilUsuarioViewModelFactory(this, 1)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_usuario)
 
-        val service = PackbackService().cria(UsuarioService::class.java)
-                ?: throw IllegalArgumentException("Não foi possível instanciar o serviço")
-
-        val dao = AppDatabase.getInstance(this).usuarioDao()
-
-        val repo = UsuarioRepository(service, dao)
-
-        val viewModel = PerfilUsuarioViewModel(1, repo)
-
         viewModel.usuario.observe(this, Observer<Usuario> { usuario ->
-            usuario_nome.text = usuario.nome
+            usuario_nome.text = usuario?.nome
         })
-
-        AppDatabase.destroyInstance()
     }
 }
