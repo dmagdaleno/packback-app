c083be9ffc9d127ccec579235cfb882d0d23b21c
dmagdaleno <diegommagdaleno@gmail.com>
2019-08-23 10:20:52 -0300

    cria activity de lista de coleta

---
+++ app/src/main/java/br/com/boomerang/packbackapp/ui/activity/ListaColetaActivity.kt
@@ -0,0 +1,13 @@
+package br.com.boomerang.packbackapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.boomerang.packbackapp.R
+
+class ListaColetaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_coleta)
+    }
+}
